dd5301b3410b368a62650f703630c55c2b062ed5
deepcrazy <312.gupta@gmail.com>
2020-02-06 10:54:45 -0500

    token factory exercise-2 done

--- tokenFactory.sol
+++ tokenFactory.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.5.10;
 
 import "./myERC721.sol";
 
@@ -10,35 +10,31 @@ contract Artist is ERC721 {
     
     constructor() public {
         artist = msg.sender;
-        _tokenOwner[0] = artist;
+        _tokenOwner[0x0] = artist;
         _ownedTokensCount[artist].increment();
     }
     
+    // Modifier to check the original artist
     modifier _onlyArtist() {
         require(msg.sender == artist, "Only artist is allowed to call this function..!!");
         _;
     }
     
+    // function for creating the art work
     function createArtwork(uint hashIPFS, string memory Name) public _onlyArtist() returns (ArtWork) {
        ArtWork artContract = new ArtWork(hashIPFS, Name, artist);
        artworks[hashIPFS] = artContract;
        return artContract;
     }
     
+    // function for checking the art work
     function checkArtwork(uint hashIPFS) public _onlyArtist() view returns(bool) {
         if(artworks[hashIPFS] == ArtWork(0x0)) {
             return false;
         }
         return true;  
     }
-    
-    
-    
-    function sellArtWork(uint hashIPFS, address newOwner) public _onlyArtist() {
-        ArtWork artContract = artworks[hashIPFS];           //get the artWork contract instance
-        
-        artContract.sellArtWork(hashIPFS, newOwner);        //calling sellArtWork function to sell the art work.
-    }
+
 }
 
 contract ArtWork is ERC721 {
@@ -55,28 +51,22 @@ contract ArtWork is ERC721 {
         hashIPFS = ipfsHash;
         owner = originalOwner;
         
+        //  keep tract of owner and no of tokens for each owner..
         _tokenOwner[hashIPFS] = owner;
         _ownedTokensCount[owner].increment();
 
     }
     
-    modifier _onlyOwner() {
-        require(msg.sender == owner, "Only owner can sell the art work..!!");
-        _;
-    }
-    
+    //  function for setting the owner of the art contract
     function setOwner(address newOwner) public {
         if(owner == msg.sender) {
             owner = newOwner;
         }
     }
     
+    //  function for selling the art work..
     function  sellArtWork(uint _hashIPFS, address _to) public {
-        require(ownerOf(_hashIPFS) == owner, "User is not the owner of the art work..!!");
         require(_ownedTokensCount[owner].current() > 0, "There are no tokens of Art Work..!!");
-        
-        _ownedTokensCount[owner].decrement();
-        _ownedTokensCount[_to].increment();
         
         safeTransferFrom(owner, _to, _hashIPFS);
         setOwner(_to);
